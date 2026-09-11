6aa62fccc80f27690136dc9df4e2dbb49d43846c
HaoxiangWu <547438335@qq.com>
2026-09-11 18:34:03 +0200

    del: delete the prompt

--- src/inference/inference.py
+++ src/inference/inference.py
@@ -1,23 +1,23 @@
 from FlagEmbedding import BGEM3FlagModel
-from FlagEmbedding import FlagReranker
+# from FlagEmbedding import FlagReranker
 import torch
 
 class Inference:
     def __init__(self, embedded_model_path: str, rerank_model_path: str):
         self.embedded_model = BGEM3FlagModel(embedded_model_path, use_fp16=True)
-        self.rerank_model = FlagReranker(rerank_model_path, use_fp16=True)
+        # self.rerank_model = FlagReranker(rerank_model_path, use_fp16=True)
 
     def predict(self, input_data: list):
         return 
 
+    def preprocess(self, input_data: str):
+        return
+
     def __call__(self, input_data: list):
         return self.predict(input_data)
 
-    def Embedder(self, input_data: list):
+    def embedder(self, input_data: list):
         return
 
-    def Retriever(self, input_data: list):
+    def retriever(self, input_data: list):
         return 
-
-    def Decoder(self, input_data: list):
-        return
